7458e69e83ce27480052d7b5f1ad917ef24f021d
Marlody Rodriguez <marlodycairo@hotmail.com>
2022-04-24 02:58:27 -0500

    Actualización

---
+++ TecnicalTestLibrary.Api.Tests/AuthorDomainTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace TecnicalTestLibrary.Api.Tests
+{
+    public class AuthorDomainTests
+    {
+        [Fact]
+        public void CreateNewAuthorShouldInsert()
+        {
+            //Arrange - organizar
+            //Act - actuar
+            //Assert - afirmar
+        }
+    }
+}
